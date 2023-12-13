5b63610e1d1e23a727e03f151ba4a7430b58b07f
narramuni <nmspavan@gmail.com>
2023-12-13 11:07:24 +0530

    first commit

---
+++ AzureRainbow/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AzureRainbow
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+                string connectionString = ConfigurationManager.ConnectionStrings["Server=tcp:saipavan.database.windows.net,1433;" +
+                    "Initial Catalog=RainbowSql;Persist Security Info=False;" +
+                    "User ID=SaipavanSql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;" +
+                    "TrustServerCertificate=False;Connection Timeout=30;"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+                    
+                }
+            }
+        }
+    }
+}
